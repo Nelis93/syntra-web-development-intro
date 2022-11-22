52b73dc00e36d6e299113ad89dba1df7fb57bbb0
Nelis <neliseerdekens1@hotmail.com>
2022-11-22 21:11:15 +0100

    tussentijdse push

--- 4. Javascript Classes/2. Data Structures/3. Queue.js
+++ 4. Javascript Classes/2. Data Structures/3. Queue.js
@@ -10,16 +10,24 @@ class Queue {
    * Enqueues a new value at the end of the queue
    * @param {*} value the value to enqueue
    */
-  enqueue() {}
+  enqueue(value) {
+    this._storage.shift(value);
+    return this;
+  }
 
   /*
    * Dequeues the value from the beginning of the queue and returns it
    * @return {*} the first and oldest value in the queue
    */
-  dequeue() {}
+  dequeue() {
+    this._storage.unshift();
+    return this;
+  }
   /*
    * Returns the value at the beginning of the queue without removing it from the queue
    * @return {*} the first and oldest value in the queue
    */
-  peek() {}
+  peek() {
+    return this._storage[0];
+  }
 }
--- 4. Javascript Classes/2. Data Structures/7. Stack.js
+++ 4. Javascript Classes/2. Data Structures/7. Stack.js
@@ -1,27 +1,38 @@
 /** Class representing a Stack. */
 
 class Stack {
+  #storage;
   constructor() {
-    this._storage = [];
+    this.#storage = [];
   }
   /*
    * Adds a new value at the end of the stack
    * @param {*} value the value to push
    */
-  push(value) {}
+  push(value) {
+    this.#storage.push(value);
+    return this;
+  }
 
   /*
    * Removes the value at the end of the stack and returns it
    * @return {*} the last and newest value in the stack
    */
-  pop() {}
+  pop() {
+    this.#storage.pop();
+    return this;
+  }
   /*
    * Returns the value at the end of the stack without removing it
    * @return {*} the last and newest value in the stack
    */
-  peek() {}
+  peek() {
+    this.#storage[-1];
+    return this;
+  }
 }
 
 const myStack = new Stack();
 
 console.log(myStack);
+window.Stack = Stack;
